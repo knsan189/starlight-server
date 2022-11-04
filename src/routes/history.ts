a4ab054d031c4ee9b55e5a9447556bb29afbbe44
DESKTOP-8F9VMMN\hnJin <knsan189@gmail.com>
2022-11-04 14:09:27 +0900

    member sync api 추가

--- src/routes/history.ts
+++ src/routes/history.ts
@@ -1,5 +1,5 @@
 import { Request, Response, Router } from "express";
-import { DiscordHistory } from "../@types/types";
+import { DiscordHistory, DiscordMember } from "../@types/types";
 import HistoryService from "../services/history.js";
 import MemberService from "../services/member.js";
 
@@ -37,5 +37,71 @@ HistoryRouter.get("/", async (req: Request<{}, {}, {}, QueryString>, res) => {
     res.status(500).send(error);
   }
 });
+
+interface SyncRequestBody {
+  nicknames: DiscordMember["nickname"][];
+}
+
+HistoryRouter.post(
+  "/sync",
+  async (req: Request<{}, {}, SyncRequestBody>, res) => {
+    try {
+      const { nicknames } = req.body;
+      const dbMembers = await MemberService.getMembers();
+      const promiseArray = [];
+      const time = new Date().toString();
+
+      nicknames.forEach((nickname) => {
+        const target = dbMembers.find((member) => member.nickname === nickname);
+        if (target) {
+          console.log(
+            `오프라인으로 저장되었는데, 온라인 상태인 유저 [${target.nickname}]`
+          );
+          const joinTime = new Date(target.lastJoinedTime).getTime();
+          const leaveTime = new Date(target.lastLeaveTime).getTime();
+
+          // 종료시간이 접속시간보다 큰데, 접속 중인 경우
+          if (leaveTime > joinTime) {
+            promiseArray.push(
+              MemberService.editMember({
+                nickname,
+                type: "join",
+                time,
+              })
+            );
+          }
+        } else {
+          console.log(`미등록 유저 [${target.nickname}]`);
+          promiseArray.push(
+            MemberService.addMember({ nickname, type: "join", time })
+          );
+        }
+      });
+
+      dbMembers.forEach((member) => {
+        const joinTime = new Date(member.lastJoinedTime).getTime();
+        const leaveTime = new Date(member.lastLeaveTime).getTime();
+
+        if (joinTime > leaveTime && !nicknames.includes(member.nickname)) {
+          console.log(
+            `온라인으로 저장되었는데 오프라인 상태인 유저 [${member.nickname}]`
+          );
+          promiseArray.push(
+            MemberService.editMember({
+              nickname: member.nickname,
+              type: "leave",
+              time,
+            })
+          );
+        }
+      });
+
+      await Promise.all(promiseArray);
+      res.send("ok");
+    } catch (error) {
+      res.status(500).send(error);
+    }
+  }
+);
 
 export default HistoryRouter;

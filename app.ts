42af8d804843906f62932dbcc66da0e97f84acbb
hnjin <knsan189@gmail.com>
2022-12-15 14:41:39 +0900

    app.js 환경 변수수정

--- app.ts
+++ app.ts
@@ -15,7 +15,7 @@ import FortuneRouter from "./src/routes/fortune.js";
 import HistoryRouter from "./src/routes/history.js";
 import MapRouter from "./src/routes/map.js";
 
-const isDev = process.env.NODE_ENV.trim() !== "production";
+const isDev = `${process.env.NODE_ENV}`.trim() === "development";
 const app = express();
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);

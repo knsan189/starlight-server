445fb91305dc83f679031a0d55942287f41d11ea
hnjin <knsan189@gmail.com>
2023-01-05 09:24:33 +0900

    운세 답변 로거 추가

--- src/routes/message.ts
+++ src/routes/message.ts
@@ -5,7 +5,6 @@ import { formatDistanceToNow } from "date-fns";
 import ko from "date-fns/locale/ko/index.js";
 import { MessageRequest, MessageResponse } from "../@types/message";
 import { DiscordMember } from "../@types/types";
-import userScraper from "../utils/userScraper.js";
 import MemberService from "../services/member.js";
 import FortuneService from "../services/fortune.js";
 
@@ -38,6 +37,7 @@ async function shuffleFortuneArray(): Promise<void> {
 MessageRouter.post("/", async (req, res) => {
   try {
     const { msg, sender }: MessageRequest = req.body;
+
     let parsedSender = getParsedSender(sender);
 
     if (parsedSender.length > 2) {
@@ -74,21 +74,26 @@ MessageRouter.post("/", async (req, res) => {
         return res.send(response);
       }
 
-      const fortune = await FortuneService.getFortune(index);
+      const { fortune, msg, delayTime } = await FortuneService.getFortune(index);
+      const reply = fortune.format(parsedSender);
+      const secondReply = msg?.format(parsedSender);
 
       const response: MessageResponse = {
         status: "ok",
-        reply: fortune.fortune.format(parsedSender),
-        secondReply: fortune.msg?.format(parsedSender),
-        delayTime: fortune.delayTime,
+        reply,
+        secondReply,
+        delayTime,
       };
+
+      logger.info(`별빛 : ${reply}`);
+      logger.info(`별빛 : ${secondReply}`);
       return res.send(response);
     }
 
-    if (msg.indexOf("/유저") === 0) {
-      const nickname = msg.replace("/유저", "").trim();
-      const userData = await userScraper(nickname);
-    }
+    // if (msg.indexOf("/유저") === 0) {
+    //   const nickname = msg.replace("/유저", "").trim();
+    //   const userData = await userScraper(nickname);
+    // }
 
     if (msg.includes("승호") && msg.includes("언제")) {
       const member: DiscordMember = await MemberService.getMember("승호");

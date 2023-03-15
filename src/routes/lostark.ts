288ca7e17530ace5701b83e7d90e379a96b9ef1e
hnjin <knsan189@gmail.com>
2023-03-15 13:06:36 +0900

    모험섬 결과 값 저장

--- src/routes/lostark.ts
+++ src/routes/lostark.ts
@@ -3,6 +3,7 @@ import { Router } from "express";
 import LostarkService from "../services/lostark.js";
 
 const LostarkRouter = Router();
+let timeStamp = new Date();
 
 LostarkRouter.get("/guardian", async (req, res) => {
   try {
@@ -22,37 +23,48 @@ LostarkRouter.get("/abyss", async (req, res) => {
   }
 });
 
+const ResultMap = new Map<string, Calendar[]>();
+
 LostarkRouter.get("/calendar", async (req, res) => {
   try {
-    const today = format(new Date(), "yyyy-MM-dd");
-    const response = await LostarkService.getCalendar();
-    const calenders: Calendar[] = [];
-    response.forEach((content) => {
-      if (
-        content.StartTimes.some((time) => time.includes(today)) &&
-        content.CategoryName !== "로웬" &&
-        content.CategoryName !== "섬" &&
-        content.CategoryName !== "항해"
-      ) {
-        const items: Calendar["RewardItems"] = content.RewardItems.filter(
-          (item) =>
-            (item.Name === "실링" ||
-              item.Name === "해적 주화" ||
-              item.Name === "골드" ||
-              item.Name.includes("카드 팩")) &&
-            item.StartTimes,
-        ).map((item) => ({
-          ...item,
-          StartTimes: item.StartTimes?.filter((time) => time.includes(today)) || [],
-        }));
+    if (timeStamp.getDate() !== new Date().getDate()) {
+      ResultMap.clear();
+    }
 
-        calenders.push({
-          ...content,
-          StartTimes: content.StartTimes.filter((time) => time.includes(today)),
-          RewardItems: items,
-        });
-      }
-    });
+    const today = format(new Date(), "yyyy-MM-dd");
+    let calenders = ResultMap.get(today);
+
+    if (!calenders) {
+      const response = await LostarkService.getCalendar();
+      calenders = [];
+      response.forEach((content) => {
+        if (
+          content.StartTimes.some((time) => time.includes(today)) &&
+          content.CategoryName !== "로웬" &&
+          content.CategoryName !== "섬" &&
+          content.CategoryName !== "항해"
+        ) {
+          const items: Calendar["RewardItems"] = content.RewardItems.filter(
+            (item) =>
+              (item.Name === "실링" ||
+                item.Name === "해적 주화" ||
+                item.Name === "골드" ||
+                item.Name.includes("카드 팩")) &&
+              item.StartTimes,
+          ).map((item) => ({
+            ...item,
+            StartTimes: item.StartTimes?.filter((time) => time.includes(today)) || [],
+          }));
+          calenders?.push({
+            ...content,
+            StartTimes: content.StartTimes.filter((time) => time.includes(today)),
+            RewardItems: items,
+          });
+        }
+      });
+      ResultMap.set(today, calenders);
+    }
+
     return res.send(calenders);
   } catch (error) {
     console.log(error);

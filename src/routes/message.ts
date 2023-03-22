33a0af25058614e4377d48f1306c72211a20b402
hnjin <knsan189@gmail.com>
2023-03-22 13:22:36 +0900

    cheerio 제거

--- src/routes/message.ts
+++ src/routes/message.ts
@@ -1,212 +1,212 @@
-import { Router } from "express";
-import pkg from "lodash";
-import log4js from "log4js";
-import { formatDistanceToNow } from "date-fns";
-import ko from "date-fns/locale/ko/index.js";
-import { MessageRequest, MessageResponse } from "../@types/message";
-import { DiscordMember } from "../@types/types";
-import MemberService from "../services/member.js";
-import FortuneService from "../services/fortune.js";
-import LostarkService from "../services/lostark.js";
-import { io } from "../socket.js";
+// import { Router } from "express";
+// import pkg from "lodash";
+// import log4js from "log4js";
+// import { formatDistanceToNow } from "date-fns";
+// import ko from "date-fns/locale/ko/index.js";
+// import { MessageRequest, MessageResponse } from "../@types/message";
+// import { DiscordMember } from "../@types/types";
+// import MemberService from "../services/member.js";
+// import FortuneService from "../services/fortune.js";
+// import LostarkService from "../services/lostark.js";
+// import { io } from "../socket.js";
 
-const { shuffle } = pkg;
-const MessageRouter = Router();
-const logger = log4js.getLogger("message");
+// const { shuffle } = pkg;
+// const MessageRouter = Router();
+// const logger = log4js.getLogger("message");
 
-let timeStamp = new Date();
-let fortuneIndexArray: number[] = [];
-const fortuneSet = new Set<string>();
+// let timeStamp = new Date();
+// let fortuneIndexArray: number[] = [];
+// const fortuneSet = new Set<string>();
 
-function getParsedSender(sender: string) {
-  return sender.split("/")[0].trim();
-}
+// function getParsedSender(sender: string) {
+//   return sender.split("/")[0].trim();
+// }
 
-String.prototype.format = function (...args: string[]) {
-  return this.replace(/{([0-9]+)}/g, function (match, index) {
-    return typeof args[index] === "undefined" ? match : args[index];
-  });
-};
+// String.prototype.format = function (...args: string[]) {
+//   return this.replace(/{([0-9]+)}/g, function (match, index) {
+//     return typeof args[index] === "undefined" ? match : args[index];
+//   });
+// };
 
-async function shuffleFortuneArray(): Promise<void> {
-  const count = await FortuneService.getCount();
-  for (let i = 1; i <= count; i++) {
-    fortuneIndexArray.push(i);
-  }
-  fortuneIndexArray = shuffle(fortuneIndexArray);
-}
+// async function shuffleFortuneArray(): Promise<void> {
+//   const count = await FortuneService.getCount();
+//   for (let i = 1; i <= count; i++) {
+//     fortuneIndexArray.push(i);
+//   }
+//   fortuneIndexArray = shuffle(fortuneIndexArray);
+// }
 
-MessageRouter.post("/", async (req, res) => {
-  try {
-    const { msg, sender, imageDB }: MessageRequest = req.body;
+// MessageRouter.post("/", async (req, res) => {
+//   try {
+//     const { msg, sender, imageDB }: MessageRequest = req.body;
 
-    io.emit("message", { msg, sender, date: new Date().toString(), imageDB });
+//     io.emit("message", { msg, sender, date: new Date().toString(), imageDB });
 
-    let parsedSender = getParsedSender(sender);
+//     let parsedSender = getParsedSender(sender);
 
-    if (parsedSender.length > 2) {
-      parsedSender = parsedSender.substring(1);
-    }
+//     if (parsedSender.length > 2) {
+//       parsedSender = parsedSender.substring(1);
+//     }
 
-    logger.level = "debug";
-    logger.info(`${sender} : ${msg}`);
+//     logger.level = "debug";
+//     logger.info(`${sender} : ${msg}`);
 
-    if (msg.indexOf("/메시지") === 0) {
-      const response: MessageResponse = {
-        status: "ok",
-        reply: msg.replace("/메세지", "").trim(),
-        secondReply: "5초후 답장 테스트",
-        delayTime: 5000,
-      };
-      return res.send(response);
-    }
+//     if (msg.indexOf("/메시지") === 0) {
+//       const response: MessageResponse = {
+//         status: "ok",
+//         reply: msg.replace("/메세지", "").trim(),
+//         secondReply: "5초후 답장 테스트",
+//         delayTime: 5000,
+//       };
+//       return res.send(response);
+//     }
 
-    if (msg === "/운세") {
-      if (!fortuneIndexArray.length || timeStamp.getDate() !== new Date().getDate()) {
-        await shuffleFortuneArray();
-        timeStamp = new Date();
-        fortuneSet.clear();
-      }
+//     if (msg === "/운세") {
+//       if (!fortuneIndexArray.length || timeStamp.getDate() !== new Date().getDate()) {
+//         await shuffleFortuneArray();
+//         timeStamp = new Date();
+//         fortuneSet.clear();
+//       }
 
-      const index = fortuneIndexArray.shift();
+//       const index = fortuneIndexArray.shift();
 
-      if (fortuneSet.has(parsedSender)) {
-        const response: MessageResponse = {
-          status: "ok",
-          reply: "운세는 하루에 한번만 사용 하실수 있습니다.",
-        };
-        return res.send(response);
-      }
+//       if (fortuneSet.has(parsedSender)) {
+//         const response: MessageResponse = {
+//           status: "ok",
+//           reply: "운세는 하루에 한번만 사용 하실수 있습니다.",
+//         };
+//         return res.send(response);
+//       }
 
-      let fortune = "";
-      let msg = "";
-      let delayTime: number | undefined;
+//       let fortune = "";
+//       let msg = "";
+//       let delayTime: number | undefined;
 
-      while (fortune.trim().length === 0) {
-        const response = await FortuneService.getFortune(index || 0);
-        fortune = response.fortune;
-        msg = response.msg || "";
-        delayTime = response.delayTime;
-      }
+//       while (fortune.trim().length === 0) {
+//         const response = await FortuneService.getFortune(index || 0);
+//         fortune = response.fortune;
+//         msg = response.msg || "";
+//         delayTime = response.delayTime;
+//       }
 
-      const reply = fortune.format(parsedSender);
-      const secondReply = msg?.format(parsedSender);
+//       const reply = fortune.format(parsedSender);
+//       const secondReply = msg?.format(parsedSender);
 
-      const response: MessageResponse = {
-        status: "ok",
-        reply,
-        secondReply,
-        delayTime,
-      };
+//       const response: MessageResponse = {
+//         status: "ok",
+//         reply,
+//         secondReply,
+//         delayTime,
+//       };
 
-      logger.info(`별빛 : ${reply}`);
+//       logger.info(`별빛 : ${reply}`);
 
-      if (secondReply) {
-        logger.info(`별빛 : ${secondReply}`);
-      }
+//       if (secondReply) {
+//         logger.info(`별빛 : ${secondReply}`);
+//       }
 
-      fortuneSet.add(parsedSender);
-      return res.send(response);
-    }
+//       fortuneSet.add(parsedSender);
+//       return res.send(response);
+//     }
 
-    // if (msg.indexOf("/유저") === 0) {
-    //   const nickname = msg.replace("/유저", "").trim();
-    //   const userData = await userScraper(nickname);
-    // }
+//     // if (msg.indexOf("/유저") === 0) {
+//     //   const nickname = msg.replace("/유저", "").trim();
+//     //   const userData = await userScraper(nickname);
+//     // }
 
-    if (msg.includes("승호") && msg.includes("언제")) {
-      const member: DiscordMember = await MemberService.getMember("승호");
+//     if (msg.includes("승호") && msg.includes("언제")) {
+//       const member: DiscordMember = await MemberService.getMember("승호");
 
-      if (!member) throw new Error("no user found");
+//       if (!member) throw new Error("no user found");
 
-      const { lastJoinedTime, lastLeaveTime } = member;
+//       const { lastJoinedTime, lastLeaveTime } = member;
 
-      if (!lastJoinedTime || !lastLeaveTime) {
-        return;
-      }
+//       if (!lastJoinedTime || !lastLeaveTime) {
+//         return;
+//       }
 
-      if (new Date(lastJoinedTime).getTime() > new Date(lastLeaveTime).getTime()) {
-        const date = formatDistanceToNow(new Date(lastJoinedTime), {
-          addSuffix: true,
-          locale: ko,
-        });
+//       if (new Date(lastJoinedTime).getTime() > new Date(lastLeaveTime).getTime()) {
+//         const date = formatDistanceToNow(new Date(lastJoinedTime), {
+//           addSuffix: true,
+//           locale: ko,
+//         });
 
-        return res.send({
-          status: "ok",
-          reply: `지금 접속중이신걸요?`,
-          secondReply: `${date}에 접속하셔서 아직 계십니담. 또 언제 사라지실지는 모르겠지만요`,
-        });
-      }
+//         return res.send({
+//           status: "ok",
+//           reply: `지금 접속중이신걸요?`,
+//           secondReply: `${date}에 접속하셔서 아직 계십니담. 또 언제 사라지실지는 모르겠지만요`,
+//         });
+//       }
 
-      const date = formatDistanceToNow(new Date(lastLeaveTime), {
-        addSuffix: true,
-        locale: ko,
-      });
+//       const date = formatDistanceToNow(new Date(lastLeaveTime), {
+//         addSuffix: true,
+//         locale: ko,
+//       });
 
-      return res.send({
-        status: "ok",
-        reply: `디코 ${date}에 마지막으로 접속하시구`,
-        secondReply: "다시 안오셨어요 ㅠㅠ",
-      });
-    }
+//       return res.send({
+//         status: "ok",
+//         reply: `디코 ${date}에 마지막으로 접속하시구`,
+//         secondReply: "다시 안오셨어요 ㅠㅠ",
+//       });
+//     }
 
-    if (msg.includes("디코") && msg.includes("누구") && msg.includes("지금")) {
-      const members = await MemberService.getMembers();
-      const currentUser: DiscordMember[] = [];
+//     if (msg.includes("디코") && msg.includes("누구") && msg.includes("지금")) {
+//       const members = await MemberService.getMembers();
+//       const currentUser: DiscordMember[] = [];
 
-      members.forEach((member) => {
-        if (member.lastLeaveTime && member.lastJoinedTime) {
-          const joinTime = new Date(member.lastJoinedTime).getTime();
-          const leaveTime = new Date(member.lastLeaveTime).getTime();
-          if (joinTime > leaveTime) {
-            currentUser.push(member);
-          }
-        } else {
-          currentUser.push(member);
-        }
-      });
+//       members.forEach((member) => {
+//         if (member.lastLeaveTime && member.lastJoinedTime) {
+//           const joinTime = new Date(member.lastJoinedTime).getTime();
+//           const leaveTime = new Date(member.lastLeaveTime).getTime();
+//           if (joinTime > leaveTime) {
+//             currentUser.push(member);
+//           }
+//         } else {
+//           currentUser.push(member);
+//         }
+//       });
 
-      if (currentUser.length === 0) {
-        return res.send({
-          status: "ok",
-          reply: `지금 아무도 접속 안하고 있는거 같아요 !`,
-        });
-      }
+//       if (currentUser.length === 0) {
+//         return res.send({
+//           status: "ok",
+//           reply: `지금 아무도 접속 안하고 있는거 같아요 !`,
+//         });
+//       }
 
-      let reply = "지금 ";
-      currentUser.forEach((member) => {
-        reply += `${getParsedSender(member.nickname)}님 `;
-      });
+//       let reply = "지금 ";
+//       currentUser.forEach((member) => {
+//         reply += `${getParsedSender(member.nickname)}님 `;
+//       });
 
-      reply += "접속해 계시는거 같아요 !";
+//       reply += "접속해 계시는거 같아요 !";
 
-      return res.send({
-        status: "ok",
-        reply,
-      });
-    }
+//       return res.send({
+//         status: "ok",
+//         reply,
+//       });
+//     }
 
-    if (msg === "/도가토") {
-      const { Raids } = await LostarkService.getGuardians();
-      return res.send({
-        status: "ok",
-        reply: `이번주 도전 가디언 토벌은 : \n ${Raids.map((r) => r.Name).join(", ")} 입니다.`,
-      });
-    }
+//     if (msg === "/도가토") {
+//       const { Raids } = await LostarkService.getGuardians();
+//       return res.send({
+//         status: "ok",
+//         reply: `이번주 도전 가디언 토벌은 : \n ${Raids.map((r) => r.Name).join(", ")} 입니다.`,
+//       });
+//     }
 
-    if (msg === "/도비스") {
-      const abyss = await LostarkService.getAbyss();
-      return res.send({
-        status: "ok",
-        reply: `이번주 도전 어비스 지역은 : \n ${abyss[0].AreaName} 입니다.`,
-      });
-    }
+//     if (msg === "/도비스") {
+//       const abyss = await LostarkService.getAbyss();
+//       return res.send({
+//         status: "ok",
+//         reply: `이번주 도전 어비스 지역은 : \n ${abyss[0].AreaName} 입니다.`,
+//       });
+//     }
 
-    return res.send("ok");
-  } catch (error) {
-    logger.error(error);
-    return res.send({ status: "error", reply: "에러났어요 ㅠ" + error });
-  }
-});
+//     return res.send("ok");
+//   } catch (error) {
+//     logger.error(error);
+//     return res.send({ status: "error", reply: "에러났어요 ㅠ" + error });
+//   }
+// });
 
-export default MessageRouter;
+// export default MessageRouter;
